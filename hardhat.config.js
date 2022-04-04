0045684984de67dced5b541140e48c651818c5c3
Isamu <yunchunnan@qq.com>
2022-04-04 14:03:16 -0700

    First Blood

---
+++ hardhat.config.js
@@ -0,0 +1,30 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+const fs = require("fs")
+const privateKey = fs.readFileSync(".secret").toString()
+const projectId = "f190c0987c5b4532a2a14bc95ef1d51a"
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    }
+  },
+  solidity: "0.8.4",
+};
